c23698fb044a82bfc5a2a9dff8fe31636c4e8d8e
Joe SOTAbot <sotabots@gmail.com>
2024-01-23 14:54:53 -0800

    Drive Subsystem complete

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public class SwerveModule {
+
+    public void setModule(SwerveModuleState swerveModuleState) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setModule'");
+    }
+    
+}
